4bb3bb1b26b19d82cc90097abee7e1e1aa2a3988
jcelsi <javier.ortizc@sansano.usm.cl>
2020-11-27 22:43:13 -0300

    cambio de contraseña

--- server/routes/users.js
+++ server/routes/users.js
@@ -15,4 +15,34 @@ router.get("/profile",async (req,res)=>{
         return res.status(400).send(err);
     }
 });
+
+router.post("/passUpdate",async (req,res)=>{
+  try {
+      const user = await Users.findOne({
+          where: {
+            id: req.header("userId"),
+          },
+        });
+        
+        const validPass = await bcrypt.compare(req.body.oldPassword, user.password);
+        if (!validPass) return res.status(401).send({message:"constraseña antigua incorrecta"});
+
+        //hasheo nueva contraseña
+        const salt = await bcrypt.genSalt(10);
+        const hashPass = await bcrypt.hash(req.body.newPassword, salt);    
+        
+        const user = await Users.update({
+          password:hashPass,
+        },{
+          where: {
+            id: req.header("userId"),
+          },
+        });
+
+      return res.status(200).send({message:"constraseña antigua incorrecta"});
+  } catch (err) {
+      return res.status(400).send(err);
+  }
+});
+
 module.exports = router;
