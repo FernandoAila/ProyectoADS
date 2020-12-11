867408b32a291ee6be03331938db638af27a6b06
jcelsi <javier.ortizc@sansano.usm.cl>
2020-12-11 09:13:52 -0300

    Modulos no cotiazdos 2.0

--- client/src/components/modulos_no_cotizados.js
+++ client/src/components/modulos_no_cotizados.js
@@ -1,9 +1,25 @@
 import axios from "axios";
 import React, { useState, useRef,useEffect} from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+});
+  
 
 const NoCotizadosModulos=()=>{
 
     const [modulos, setModulos] = useState([]);
+    const classes = useStyles();
     useEffect(() => {
         axios.get("http://localhost:8080/modulesRoutes/AllUnasigned",
         {
@@ -13,18 +29,37 @@ const NoCotizadosModulos=()=>{
       });
       }, []);
     return(
-        <div>
-                    <h1>Modulos no cotizados</h1>
-            {modulos.map( (modulo,index) => (
-                <div>
-                    <strong>Nombre</strong>
-                <li 
-                className="modulo" key={index}>{modulo.nameModule}</li>
-                <strong>Descripcion</strong>
-                                <div
-                className="modulo" key={index}>{modulo.descriptionModule}</div>
+        <div class="main-container">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-11 col-xl-10">
+                        <div class="tab-content">                       
+                            <div class="content-list-body">
+                                <div class="card-list">
+                                    <div class="card-list-head">
+                                        <h3>Modulos no cotizados</h3>
+                                    </div>
+                                    <div class="card-list-body-fklter-list-1607686290867">
+                                        {modulos.map( (modulo,index) => (
+                                            <div class="card card-task">
+                                                <div class="card-body">
+                                                    <div class="card-title">
+                                                        {modulo.nameModule}
+                                                        <hr></hr>
+                                                        <span class="text-small">
+                                                            <p>{modulo.descriptionModule}</p>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            ))}
+            </div>
         </div>
     );
 }
